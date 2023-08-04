c496486034c4c920236c649590e9a0cb2c25dedf
Damien <damienrahari@gmail.com>
2023-08-04 11:22:48 +0200

    Initial commit

---
+++ src/test/java/fr/dawan/demojenkinsmvc/DemoJenkinsMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.dawan.demojenkinsmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJenkinsMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
